3f1e59dbd60d0ed9b39206d5b1c8e4e45555afa5
Code12naman <144878132+Code12naman@users.noreply.github.com>
2023-09-20 18:32:05 +0530

    Add files via upload

---
+++ day 3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main (){
+  
+  float radius , area;
+  printf ("enter the radius :");
+  scanf("%f",&radius);
+     area=3.14*radius*radius;
+  printf("area of circle is %f",area);
+     
+
+
+
+
+    return 0;
+}
