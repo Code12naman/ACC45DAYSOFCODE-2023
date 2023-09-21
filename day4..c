0494e86a9cdd4fbad57e17fec898992042b4ba3c
Code12naman <144878132+Code12naman@users.noreply.github.com>
2023-09-21 17:48:34 +0530

    Add files via upload

---
+++ day4..c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main (){
+     int a ;
+     printf("enter your age ");
+     scanf("%d",&a);
+
+     if (a>18)
+     {
+        printf("you are adult  ");
+       
+     }else{
+        printf("you are not adult");
+       
+     }
+     
+
+    return 0;
+}
