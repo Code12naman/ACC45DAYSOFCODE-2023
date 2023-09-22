386c85666b68faafedc4ffc0b3b1fa42336289e2
Code12naman <144878132+Code12naman@users.noreply.github.com>
2023-09-22 19:20:57 +0530

    Add files via upload

---
+++ day5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("enter the battery level");
+    scanf("%d", &a);
+ if (a<=15)
+ {
+    printf("yes");
+ }
+ else  
+ {
+    printf("no ");
+ }
+ return 0;
+}
